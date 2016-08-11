d5a780714e63b427af296d12f464ea280906ee90
EvaLok <squidstock@hotmail.com>
2016-08-11 15:03:40 -0600

    multiple handlers test

--- spec/EventRouter/v1/RouterSpec.php
+++ spec/EventRouter/v1/RouterSpec.php
@@ -42,4 +42,47 @@ class RouterSpec extends ObjectBehavior
 
 	    $results['test.handler.1']['count']->shouldEqual(16);
     }
+
+	function it_should_register_multiple_event_handlers_properly()
+	{
+		$handler1 = (new Handler(
+			'test.handler.1',
+			function( $data ){
+				$data['count'] += 15;
+
+				return $data;
+			})
+		);
+
+		$handler2 = (new Handler(
+			'test.handler.2',
+			function( $data ){
+				$data['count'] += 100;
+
+				return $data;
+			})
+		);
+
+		$this->registerHandler(['test.event.1'], $handler1);
+		$this->registerHandler(['test.event.1'], $handler2);
+
+		$handlers = $this->getHandlers();
+
+		$handlers['test.event.1'][0]
+			->shouldHaveType('EventRouter\v1\Handler');
+		$handlers['test.event.1'][1]
+			->shouldHaveType('EventRouter\v1\Handler');
+
+		$handlers['test.event.1'][0]->getName()
+			->shouldBe('test.handler.1');
+		$handlers['test.event.1'][1]->getName()
+			->shouldBe('test.handler.2');
+
+		$results = $this->handleEvent(
+			new Event('test.event.1', ['count' => 1])
+		);
+
+		$results['test.handler.1']['count']->shouldEqual(16);
+		$results['test.handler.2']['count']->shouldEqual(101);
+	}
 }
